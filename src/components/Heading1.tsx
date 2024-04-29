258a719066846c766dbd2412b1bf0869d6828115
motoshira <51407064+motoshira@users.noreply.github.com>
2024-04-30 00:20:51 +0900

    Use h1 in Heading1

--- src/components/Heading1.tsx
+++ src/components/Heading1.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Heading1 = ({ children }: { children: React.ReactNode }) => (
-  <h2 className="text-2xl font-bold text-gray-600 lg:text-4xl">{children}</h2>
+  <h1 className="text-2xl font-bold text-gray-600 lg:text-4xl">{children}</h1>
 );
 
 export default Heading1;
