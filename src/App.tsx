e1d05eb71a652fdd638c3d3a9c4862dc77b409ed
motoshira <51407064+motoshira@users.noreply.github.com>
2025-07-13 18:24:38 +0900

    Migrate to biome from eslint/prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,171 +1,171 @@
-import Section from '@/components/Section';
-import Heading1 from '@/components/Heading1';
-import Heading2 from '@/components/Heading2';
-import Heading3 from '@/components/Heading3';
-import Text1 from '@/components/Text1';
-import SectionWrap from '@/components/SectionWrap';
-import SectionItem from '@/components/SectionItem';
-import LinkText from '@/components/LinkText';
-import LinkWrap from './components/linkWrap';
+import Heading1 from "@/components/Heading1";
+import Heading2 from "@/components/Heading2";
+import Heading3 from "@/components/Heading3";
+import LinkText from "@/components/LinkText";
+import Section from "@/components/Section";
+import SectionItem from "@/components/SectionItem";
+import SectionWrap from "@/components/SectionWrap";
+import Text1 from "@/components/Text1";
+import LinkWrap from "./components/linkWrap";
 
 const Navigation = () => {
-  return (
-    <nav className="sticky top-0 flex h-[60px] items-center bg-primary shadow-md z-10">
-      <a
-        href="/"
-        className="ml-6 text-xl font-bold decoration-2 hover:underline"
-      >
-        motoshira.net
-      </a>
-    </nav>
-  );
+	return (
+		<nav className="sticky top-0 flex h-[60px] items-center bg-primary shadow-md z-10">
+			<a
+				href="/"
+				className="ml-6 text-xl font-bold decoration-2 hover:underline"
+			>
+				motoshira.net
+			</a>
+		</nav>
+	);
 };
 
 const SubSectionWrap = ({ children }: { children: React.ReactNode }) => {
-  return <div className="flex flex-col w-full space-y-4">{children}</div>;
+	return <div className="flex flex-col w-full space-y-4">{children}</div>;
 };
 
 function App() {
-  return (
-    <div className="h-full w-full relative">
-      <div className="top-0 left-0 h-full w-full absolute">
-        <Navigation />
-        <main className="relative">
-          <SectionWrap>
-            <Section>
-              <img
-                height="312"
-                width="312"
-                src="/me.webp"
-                className="mx-auto rounded-[50%] shadow-md"
-                alt="my Logo"
-              />
-              <Heading1>motoshira</Heading1>
-              <p className="text-gray-600 text-lg lg:text-xl">Kohei Hosoki</p>
-            </Section>
-          </SectionWrap>
-          <SectionWrap>
-            <Section>
-              <Heading2>Works</Heading2>
-              <SubSectionWrap>
-                <LinkWrap href="https://main.d29mqqgyf3frw5.amplifyapp.com">
-                  <Heading3>WebGL School 2024 Homework</Heading3>
-                </LinkWrap>
-                <p>
-                  <LinkText href="https://webgl.souhonzan.org/entry/?v=2635">
-                    WebGLスクール第11期
-                  </LinkText>
-                  <Text1>
-                    に参加させていただいた際に提出した課題一覧です。
-                  </Text1>
-                </p>
-              </SubSectionWrap>
-              <SubSectionWrap>
-                <LinkWrap href="https://gist.github.com/motoshira/806c3e5ebe3a929e8a5751ebad372cfb">
-                  <Heading3>
-                    クレジットカード明細の項目を分類するスクリプト
-                  </Heading3>
-                </LinkWrap>
-                <p>
-                  <Text1>
-                    N-gramと単純ベイズを利用して、クレジットカード明細の項目を分類します。
-                  </Text1>
-                </p>
-              </SubSectionWrap>
-            </Section>
-          </SectionWrap>
-          <SectionWrap>
-            <Section>
-              <Heading2>Experience</Heading2>
-              <ul>
-                <li>
-                  <Text1>2024/4~ 株式会社L is B</Text1>
-                </li>
-                <li>
-                  <Text1>2021/4~2024/3 トヨクモ株式会社</Text1>
-                </li>
-              </ul>
-            </Section>
-          </SectionWrap>
-          <SectionWrap>
-            <Section>
-              <Heading2>Likes</Heading2>
-              <SubSectionWrap>
-                <Heading3>Web開発</Heading3>
-                <p>
-                  <Text1>
-                    Web開発全般、特にフロントエンドの領域に興味があります。
-                    <br />
-                    人の役に立つもの・使いやすいものとはどのようまものか考えること・手を動かすことが好きです。
-                    (TODO もっど詳しく言語化する)
-                  </Text1>
-                </p>
-              </SubSectionWrap>
-              <SubSectionWrap>
-                <Heading3>プログラミング言語</Heading3>
-                <p>
-                  <Text1>
-                    TypeScriptの他、Clojure/Common
-                    Lisp等のLisp系言語が好きです。
-                    <br />
-                    最近は専らTypeScriptを書いています。
-                  </Text1>
-                </p>
-              </SubSectionWrap>
+	return (
+		<div className="h-full w-full relative">
+			<div className="top-0 left-0 h-full w-full absolute">
+				<Navigation />
+				<main className="relative">
+					<SectionWrap>
+						<Section>
+							<img
+								height="312"
+								width="312"
+								src="/me.webp"
+								className="mx-auto rounded-[50%] shadow-md"
+								alt="my Logo"
+							/>
+							<Heading1>motoshira</Heading1>
+							<p className="text-gray-600 text-lg lg:text-xl">Kohei Hosoki</p>
+						</Section>
+					</SectionWrap>
+					<SectionWrap>
+						<Section>
+							<Heading2>Works</Heading2>
+							<SubSectionWrap>
+								<LinkWrap href="https://main.d29mqqgyf3frw5.amplifyapp.com">
+									<Heading3>WebGL School 2024 Homework</Heading3>
+								</LinkWrap>
+								<p>
+									<LinkText href="https://webgl.souhonzan.org/entry/?v=2635">
+										WebGLスクール第11期
+									</LinkText>
+									<Text1>
+										に参加させていただいた際に提出した課題一覧です。
+									</Text1>
+								</p>
+							</SubSectionWrap>
+							<SubSectionWrap>
+								<LinkWrap href="https://gist.github.com/motoshira/806c3e5ebe3a929e8a5751ebad372cfb">
+									<Heading3>
+										クレジットカード明細の項目を分類するスクリプト
+									</Heading3>
+								</LinkWrap>
+								<p>
+									<Text1>
+										N-gramと単純ベイズを利用して、クレジットカード明細の項目を分類します。
+									</Text1>
+								</p>
+							</SubSectionWrap>
+						</Section>
+					</SectionWrap>
+					<SectionWrap>
+						<Section>
+							<Heading2>Experience</Heading2>
+							<ul>
+								<li>
+									<Text1>2024/4~ 株式会社L is B</Text1>
+								</li>
+								<li>
+									<Text1>2021/4~2024/3 トヨクモ株式会社</Text1>
+								</li>
+							</ul>
+						</Section>
+					</SectionWrap>
+					<SectionWrap>
+						<Section>
+							<Heading2>Likes</Heading2>
+							<SubSectionWrap>
+								<Heading3>Web開発</Heading3>
+								<p>
+									<Text1>
+										Web開発全般、特にフロントエンドの領域に興味があります。
+										<br />
+										人の役に立つもの・使いやすいものとはどのようまものか考えること・手を動かすことが好きです。
+										(TODO もっど詳しく言語化する)
+									</Text1>
+								</p>
+							</SubSectionWrap>
+							<SubSectionWrap>
+								<Heading3>プログラミング言語</Heading3>
+								<p>
+									<Text1>
+										TypeScriptの他、Clojure/Common
+										Lisp等のLisp系言語が好きです。
+										<br />
+										最近は専らTypeScriptを書いています。
+									</Text1>
+								</p>
+							</SubSectionWrap>
 
-              <SubSectionWrap>
-                <Heading3>趣味</Heading3>
-                <p>
-                  <Text1>読書、カラオケ、カフェ巡り、カレー屋巡りなど</Text1>
-                </p>
-              </SubSectionWrap>
-            </Section>
-          </SectionWrap>
-          <SectionWrap>
-            <Section>
-              <Heading2>Education</Heading2>
-              <SectionItem>
-                <Text1>
-                  Bachelor of Medicine, Kanazawa University, 2015-2021
-                </Text1>
-              </SectionItem>
-            </Section>
-          </SectionWrap>
-          <SectionWrap>
-            <Section>
-              <Heading2>Links</Heading2>
-              <ul className="ml-3 list-inside">
-                {[
-                  {
-                    label: 'GitHub',
-                    url: 'https://github.com/motoshira',
-                  },
-                  {
-                    label: 'Bluesky',
-                    url: 'https://bsky.app/profile/motoshira.bsky.social',
-                  },
-                  {
-                    label: 'X (Twitter)',
-                    url: 'https://twitter.com/darjeeling743',
-                  },
-                  {
-                    label: 'AtCoder',
-                    url: 'https://atcoder.jp/users/motoshira',
-                  },
-                ].map(({ label, url }) => {
-                  return (
-                    <li key={label} className="ml-1 list-inside list-disc">
-                      <LinkText href={url}>{label}</LinkText>
-                    </li>
-                  );
-                })}
-              </ul>
-            </Section>
-          </SectionWrap>
-        </main>
-      </div>
-    </div>
-  );
+							<SubSectionWrap>
+								<Heading3>趣味</Heading3>
+								<p>
+									<Text1>読書、カラオケ、カフェ巡り、カレー屋巡りなど</Text1>
+								</p>
+							</SubSectionWrap>
+						</Section>
+					</SectionWrap>
+					<SectionWrap>
+						<Section>
+							<Heading2>Education</Heading2>
+							<SectionItem>
+								<Text1>
+									Bachelor of Medicine, Kanazawa University, 2015-2021
+								</Text1>
+							</SectionItem>
+						</Section>
+					</SectionWrap>
+					<SectionWrap>
+						<Section>
+							<Heading2>Links</Heading2>
+							<ul className="ml-3 list-inside">
+								{[
+									{
+										label: "GitHub",
+										url: "https://github.com/motoshira",
+									},
+									{
+										label: "Bluesky",
+										url: "https://bsky.app/profile/motoshira.bsky.social",
+									},
+									{
+										label: "X (Twitter)",
+										url: "https://twitter.com/darjeeling743",
+									},
+									{
+										label: "AtCoder",
+										url: "https://atcoder.jp/users/motoshira",
+									},
+								].map(({ label, url }) => {
+									return (
+										<li key={label} className="ml-1 list-inside list-disc">
+											<LinkText href={url}>{label}</LinkText>
+										</li>
+									);
+								})}
+							</ul>
+						</Section>
+					</SectionWrap>
+				</main>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/components/linkWrap.tsx
+++ src/components/linkWrap.tsx
@@ -1,21 +1,21 @@
-import { type ReactNode, type ReactElement } from 'react';
+import type { ReactElement, ReactNode } from "react";
 
 interface Props {
-  href: string;
-  children: ReactNode;
+	href: string;
+	children: ReactNode;
 }
 
 const LinkWrap = ({ href, children }: Props): ReactElement => {
-  return (
-    <a
-      href={href}
-      target="_blank"
-      rel="noopener noreferrer"
-      className="hover:opacity-70"
-    >
-      {children}
-    </a>
-  );
+	return (
+		<a
+			href={href}
+			target="_blank"
+			rel="noopener noreferrer"
+			className="hover:opacity-70"
+		>
+			{children}
+		</a>
+	);
 };
 
 export default LinkWrap;
